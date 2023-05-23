6d170e3d08fc0ffd04320cc60d7cdba88b7805c3
user07 <user07>
2023-05-23 11:13:19 +0300

    next commit

--- app/src/main/java/com/example/medicdeminakhozicheva/MainActivity6.java
+++ app/src/main/java/com/example/medicdeminakhozicheva/MainActivity6.java
@@ -2,17 +2,13 @@ package com.example.medicdeminakhozicheva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-       Intent intent = new Intent(MainActivity2.this,MainActivity7.class);
-       startActivity(intent);
+        setContentView(R.layout.activity_main6);
     }
 }
