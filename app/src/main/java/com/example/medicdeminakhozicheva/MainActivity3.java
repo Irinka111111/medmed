016b3cde41e7c96f1a06fc837533d61a52843ac2
user07 <user07>
2023-06-07 15:00:22 +0300

    next commit

--- app/src/main/java/com/example/medicdeminakhozicheva/MainActivity3.java
+++ app/src/main/java/com/example/medicdeminakhozicheva/MainActivity3.java
@@ -2,13 +2,30 @@ package com.example.medicdeminakhozicheva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+    private TextView skip1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        skip1 = findViewById(R.id.skip1);
+        skip1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+
+            public void toLogin() {
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/medicdeminakhozicheva/MainActivity5.java
+++ app/src/main/java/com/example/medicdeminakhozicheva/MainActivity5.java
@@ -1,8 +1,15 @@
 package com.example.medicdeminakhozicheva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity5 extends AppCompatActivity {
 
@@ -10,5 +17,45 @@ public class MainActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        EditText editTextTextEmailAddress = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        Button button = (Button) findViewById(R.id.button);
+        button.setBackground(ContextCompat.getDrawable(MainActivity5.this, R.drawable.button_nr));
+        button.setEnabled(false);
+
+        editTextTextEmailAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (editTextTextEmailAddress.getText().toString().contains("@") && editTextTextEmailAddress.getText().toString().contains(".")){
+                    button.setBackground(ContextCompat.getDrawable(MainActivity5.this, R.drawable.button_r));
+                    button.setEnabled(true);
+                } else if (!editTextTextEmailAddress.getText().toString().contains("@.")){
+                    button.setBackground(ContextCompat.getDrawable(MainActivity5.this, R.drawable.button_nr));
+                    button.setEnabled(false);
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+
+            public void toLogin() {
+                Intent intent = new Intent(MainActivity5.this, MainActivity7.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
